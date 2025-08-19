d89a637e52d5b01329559725290a520d292b2b70
dabananda <dabananda.dev@gmail.com>
2025-08-19 23:31:15 +0600

    feat: assign president to building

--- ApartmentManagementSystem/Controllers/AdminController.cs
+++ ApartmentManagementSystem/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using ApartmentManagementSystem.Data;
 using ApartmentManagementSystem.Models;
+using ApartmentManagementSystem.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -13,11 +14,13 @@ namespace ApartmentManagementSystem.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AdminController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        public AdminController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _context = context;
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         // GET: View of Assign President to Building
@@ -25,10 +28,10 @@ namespace ApartmentManagementSystem.Controllers
         public async Task<IActionResult> AssignPresident()
         {
             var buildings = await _context.Buildings.ToListAsync();
-            var presidents = await _userManager.GetUsersInRoleAsync("President");
-            
+            var owners = await _userManager.GetUsersInRoleAsync("Owner");
+
             ViewData["Buildings"] = new SelectList(buildings, "Id", "Name");
-            ViewData["Presidents"] = new SelectList(presidents, "Id", "UserName");
+            ViewData["Owners"] = new SelectList(owners, "Id", "UserName");
 
             return View();
         }
@@ -59,6 +62,51 @@ namespace ApartmentManagementSystem.Controllers
             ViewData["Buildings"] = new SelectList(await _context.Buildings.ToListAsync(), "Id", "Name");
             ViewData["Presidents"] = new SelectList(await _userManager.GetUsersInRoleAsync("President"), "Id", "Fullname");
             return View();
+        }
+
+        // GET: Register a new user
+        public async Task<IActionResult> RegisterUser()
+        {
+            var roles = await _roleManager.Roles.Where(r => r.Name != "SuperAdmin").ToListAsync();
+            ViewData["Roles"] = new SelectList(roles, "Name", "Name");
+            return View();
+        }
+
+        // POST: Admin/RegisterUser
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RegisterUser(RegisterUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    Fullname = model.Fullname,
+                    UserName = model.Email,
+                    Email = model.Email,
+                    EmailConfirmed = true
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, model.SelectedRole);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // THIS IS THE FIX: Re-populate the roles dropdown on validation failure
+            var roles = _roleManager.Roles.Where(r => r.Name != "SuperAdmin").ToList();
+            ViewData["Roles"] = new SelectList(roles, "Name", "Name");
+
+            // Return the view with the model to display any errors
+            return View(model);
         }
     }
 }
---
+++ ApartmentManagementSystem/ViewModels/RegisterUserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApartmentManagementSystem.ViewModels
+{
+    public class RegisterUserViewModel
+    {
+        [Required]
+        [Display(Name = "Full Name")]
+        public string Fullname { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        [Display(Name = "Role")]
+        public string SelectedRole { get; set; }
+    }
+}
