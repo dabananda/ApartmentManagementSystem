552228531e12a9b9f9ffda16cf115ea5f736ec84
dabananda <dabananda.dev@gmail.com>
2025-08-19 09:33:17 +0600

    Apartment Management System

---
+++ ApartmentManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ApartmentManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
