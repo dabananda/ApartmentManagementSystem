203b66e45e4e196ab87124772a02cd87f6532936
dabananda <dabananda.dev@gmail.com>
2025-09-07 15:25:13 +0600

    feat: Added reports

---
+++ ApartmentManagementSystem/ViewModels/Reports/OccupancyReportViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ApartmentManagementSystem.ViewModels.Reports
+{
+    public class OccupancyReportViewModel
+    {
+        public string BuildingName { get; set; } = "";
+        public int TotalFlats { get; set; }
+        public int OccupiedFlats { get; set; }
+        public int VacantFlats => TotalFlats - OccupiedFlats;
+        public decimal OccupancyRate => TotalFlats == 0 ? 0 : (decimal)OccupiedFlats / TotalFlats * 100m;
+
+        public int OwnersCount { get; set; }
+        public int TenantsCount { get; set; }
+    }
+}
