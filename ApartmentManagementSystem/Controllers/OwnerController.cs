61004cdfcbf14e3b92a74bd6abc90f429569f1f0
dabananda <dabananda.dev@gmail.com>
2025-09-11 16:52:33 +0600

    refactor: removed unused methods and views

--- ApartmentManagementSystem/Controllers/OwnerController.cs
+++ ApartmentManagementSystem/Controllers/OwnerController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
 
 namespace ApartmentManagementSystem.Controllers
 {
@@ -142,91 +141,6 @@ namespace ApartmentManagementSystem.Controllers
 
             // If you had an existing larger Dashboard model, attach vm fields there instead.
             return View(vm);
-        }
-
-        // GET: Owner/MyFlats
-        public async Task<IActionResult> MyFlats()
-        {
-            var user = await _userManager.GetUserAsync(User);
-
-            // Start with query as IQueryable<Flat> to allow Where filtering
-            var query = _context.Flats
-                .Include(f => f.Building)
-                .Include(f => f.Tenants)
-                .AsQueryable();
-
-            // Fix CS8602: Check for null user before accessing user.Id
-            if (!User.IsInRole("SuperAdmin"))
-            {
-                if (user == null)
-                {
-                    return Forbid();
-                }
-                query = query.Where(f => f.OwnerId == user.Id);
-            }
-
-            var model = await query
-                .OrderBy(f => f.FlatNumber)
-                .Select(f => new OwnerFlatsViewModel
-                {
-                    Id = f.Id,
-                    FlatNumber = f.FlatNumber,
-                    BuildingName = f.Building.Name,
-                    IsOccupied = f.Tenants.Any(t => t.IsActive),
-                    Tenants = f.Tenants.Select(t => new OwnerFlatsViewModel.TenantRow
-                    {
-                        Id = t.Id,
-                        Fullname = t.Fullname,
-                        PhoneNumber = t.PhoneNumber,
-                        IsActive = t.IsActive
-                    }).ToList()
-                })
-                .ToListAsync();
-
-            return View(model);
-        }
-
-        // POST: Owner/ToggleFlatOccupancy/{id}
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> ToggleFlatOccupancy(Guid id)
-        {
-            var user = await _userManager.GetUserAsync(User);
-            var flat = await _context.Flats.FirstOrDefaultAsync(f => f.Id == id);
-
-            if (flat == null || (flat.OwnerId != user.Id && !User.IsInRole("SuperAdmin")))
-            {
-                return Forbid();
-            }
-
-            flat.IsOccupied = !flat.IsOccupied; // Toggle the status
-            _context.Update(flat);
-            await _context.SaveChangesAsync();
-
-            return RedirectToAction(nameof(MyFlats));
-        }
-
-        // GET: Owner/MyTenants
-        public async Task<IActionResult> MyTenants()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return Forbid();
-            }
-
-            // Get all tenants from flats owned by the current user
-            var myTenants = await _context.Tenants
-                                          .Include(t => t.Flat)
-                                          .ThenInclude(f => f.Building)
-                                          .Where(t => t.Flat.OwnerId == user.Id)
-                                          .OrderBy(t => t.Flat.FlatNumber)
-                                          .ThenBy(t => t.Fullname)
-                                          .ToListAsync();
-
-            ViewData["OwnerName"] = user.Fullname;
-
-            return View(myTenants);
         }
 
         // GET: Owner/OwnedFlats
