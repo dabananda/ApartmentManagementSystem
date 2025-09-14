59ac4f9f58167f93b68ee247da7c595c69e06860
dabananda <dabananda.dev@gmail.com>
2025-09-15 00:42:01 +0600

    refactor: updated presidents dashboard

---
+++ ApartmentManagementSystem/ViewModels/President/TransactionRowViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ApartmentManagementSystem.ViewModels.President
+{
+    public class TransactionRowViewModel
+    {
+        public DateTime OccurredAt { get; set; }
+        public string Type { get; set; } = "";
+        public string Description { get; set; } = "";
+        public decimal? Amount { get; set; }
+        public string? Currency { get; set; }
+        public string Direction { get; set; } = "";
+    }
+}
